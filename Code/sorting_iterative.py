6914fb3569b19e22a7a57db4f90cc7426ea1e44a
ablades <audaris@gmail.com>
2020-03-30 19:28:42 -0700

    bubble sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -4,7 +4,7 @@
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     Running time: O(N) - Iterates over the entire array - 1
-    Memory usage: O(N) - No extra space allocated."""
+    Memory usage: O(N) - No extra space allocated. Only the array"""
     for i in range(len(items) - 1):
         if items[i] > items[i + 1]:
             return False
@@ -17,8 +17,12 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    for i in range(len(items)):
+        for j in range(len(items) - i - 1):
+            if items[j] > items[j + 1]:
+                tmp = items[j]
+                items[j] = items[j+1]
+                items[j+1] = tmp
 
 
 def selection_sort(items):
